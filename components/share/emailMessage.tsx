9f6675e22a3df100f9dd763e33f025c78eb22a36
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-05-28 12:55:52 +0200

    comit 24er

--- components/share/emailMessage.tsx
+++ components/share/emailMessage.tsx
@@ -36,9 +36,8 @@ import {
   
               <Hr className='my-[16px] mx-0 w-full' />
               <Text className='text-[#666666] text-[12px]'>
-               In a world where every email matters, Resend empowers you to send emails that captivate, engage, and convert. It's more than just an email platform; it&lsquo;s a catalyst for modernizing your email communication.
-  Don &apos;t settle for outdated email practices. Embrace the future of email communication with Resend and watch your messages soar to new heights.
-              </Text>
+               In a world where every email matters, Resend empowers you to send emails that captivate, engage, and convert. It &quot; s more than just an email platform; it&lsquo;s a catalyst for modernizing your email communication.
+  Don &apos;   </Text>
               <Text className='text-[#666666] text-[12px]'>
                 This email is delivered to you through the Resend SDK
                 integrations.✨
